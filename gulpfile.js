8f2d1be4a98b066d3acc87b53efad417e7be2f46
Michael Chen <miketychen@gmail.com>
2015-11-13 10:01:03 -0600

    gulp js scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	ejs = require('gulp-ejs'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	jshint = require('gulp-jshint'),
+	concat = require('gulp-concat');
 var path = {
-	sass:'app/scss/**/*.scss',
+	sass:'app/scss/*.scss',
 	sassOutput:'public/stylesheets',
+	js:'app/js/*.js',
+	jsOutput:'public/js',
 	ejs:'app/views/**/*.ejs',
 	ejsOutput:'public/views'
 }
+
+gulp.task('scripts', function() {
+  return gulp.src(path.js)
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(path.jsOutput));
+});
 
 gulp.task('sass',function(){
 	gulp.src(path.sass)
@@ -27,6 +38,7 @@ gulp.task('ejs',function(){
 gulp.task('watch',function(){
 	gulp.watch([path.sass],['sass']);
 	gulp.watch([path.ejs],['ejs']);
+	gulp.watch([path.js],['scripts']);
 })
 
-gulp.task('default', ['sass', 'ejs','watch']);
+gulp.task('default', ['sass', 'ejs', 'scripts','watch']);
